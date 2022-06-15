0f086f6dbdebdb577be81fca3ad25f137f811acf
ALİ CAN SAGLAM <40038623+alicansaglam@users.noreply.github.com>
2022-06-15 15:39:12 +0300

    Day1_IamRichApp
    I am following iOS & Swift Course and this is my first app on iOS.

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Ali Can Saglam on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
