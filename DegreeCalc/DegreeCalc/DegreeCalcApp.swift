3ac50dc852fd09090058c6f7bacf9b9032e26035
ALİ CAN SAGLAM <40038623+alicansaglam@users.noreply.github.com>
2022-07-11 10:34:50 +0300

    Fahrenheit Celcius Converter
    This is a very simple Fahrenheit to Celcius converter app. I am learning how SwiftUI works. Actually, it is a little bit different. I need to learn basic and common fonction that Swift has. Then, I can code what I dream.

---
+++ DegreeCalc/DegreeCalc/DegreeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  DegreeCalcApp.swift
+//  DegreeCalc
+//
+//  Created by Ali Can Saglam on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DegreeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
