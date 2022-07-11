8438c4b93c608a75a93eac462bc41ad1b9a6d924
ALİ CAN SAGLAM <40038623+alicansaglam@users.noreply.github.com>
2022-07-11 10:48:18 +0300

    Fahrenheit to Celcius Converter
    This is a very simple Fahrenheit to Celcius converter app. I am learning how SwiftUI works. Actually, it is a little bit different. I need to learn basic and common fonction that Swift has. Then, I can code what I dream.

---
+++ DegreeCalc/DegreeCalc/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  DegreeCalc
+//
+//  Created by Ali Can Saglam on 11.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var fahValue: String = ""
+    
+    let numberFormatter: NumberFormatter={
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits=1
+        numberFormatter.maximumFractionDigits=2
+        
+        return numberFormatter
+        
+    }()
+    
+    func ConverToCelcius() -> String {
+        
+        if let value = Double(fahValue){
+            
+            let fahrenheit = Measurement<UnitTemperature>(value: value , unit: .fahrenheit)
+            let celciusValue = fahrenheit.converted(to: .celsius)
+            return numberFormatter.string(from: NSNumber(value: celciusValue.value)) ?? "???"
+        }
+        else{
+            return "Awaiting..."
+        }
+       
+    }
+    
+    
+    var body: some View {
+        
+    
+        VStack{
+        
+            TextField("Fah Value?", text: $fahValue)
+                .keyboardType(.decimalPad)
+                .foregroundColor(.orange)
+                .multilineTextAlignment(.center)
+                .font(Font.system(size: 64, weight: .heavy, design: .default))
+            
+            Text("fahrenheit degrees")
+            Text("is equals to")
+            Text(ConverToCelcius()).foregroundColor(.green).multilineTextAlignment(.center).font(Font.system(size: 64, weight: .heavy, design: .default))
+            Text("Celcius Degrees")
+            Spacer()
+            Text("2022 | Ali Can Saglam").font(.system(size: 12)).fontWeight(.bold)
+        }.font(.title)
+        
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
